79f6a7c2b0c348609736b3e76855e0c899413c96
Ashish Mahindrakar <ashishmahi46@gmail.com>
2022-07-19 15:37:20 +0530

    #CardFlip: [Ashish]: WIP Animation

--- src/components/Card/Card.style.tsx
+++ src/components/Card/Card.style.tsx
@@ -1,18 +1,39 @@
+import {Animated} from 'react-native';
 import styled from 'styled-components/native';
 
 export const CardWrapper = styled.Pressable`
   margin: 10px;
-  border-width: 4px;
-  border-style: solid;
-  border-color: white;
   width: 27%;
   height: 140px;
-  justify-content: center;
-  align-items: center;
-  background-color: ${(props: {isOpen: boolean}) =>
-    props.isOpen ? 'white' : 'deepskyblue'};
 `;
+
 export const CardContent = styled.Text`
   font-size: 30px;
-  color: ${(props: {isOpen: boolean}) => (props.isOpen ? 'black' : 'white')};
+`;
+
+export const CardFront = styled(Animated.View)`
+  justify-content: center;
+  align-items: center;
+  background-color: deepskyblue;
+  position: absolute;
+  border: 4px solid white;
+  height: 100%;
+  width: 100%;
+`;
+export const CardBack = styled(Animated.View)`
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+  border: 4px solid white;
+  height: 100%;
+  width: 100%;
+`;
+
+export const CardContentFront = styled(CardContent)`
+  color: white;
+  backface-visibility: hidden;
+`;
+
+export const CardContentBack = styled(CardContent)`
+  color: black;
 `;
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
-import {CardContent, CardWrapper} from './Card.style';
+import {Animated} from 'react-native';
+import {
+  CardBack,
+  CardContent,
+  CardContentBack,
+  CardContentFront,
+  CardFront,
+  CardWrapper,
+} from './Card.style';
 
 export type CardModal = {
   id: number;
@@ -8,17 +16,66 @@ export type CardModal = {
 type CardProps = {
   card: CardModal;
   isOpen: boolean;
-  onCardPress: (id: number) => void;
+  onCardPress: () => void;
 };
 const Card: React.FC<CardProps> = ({isOpen, card, onCardPress}) => {
+  const flipAnimation = React.useRef(new Animated.Value(0)).current;
+
+  let flipRotation = 0;
+  flipAnimation.addListener(({value}) => (flipRotation = value));
+
+  const flipToFrontStyle = {
+    transform: [
+      {
+        rotateY: flipAnimation.interpolate({
+          inputRange: [0, 180],
+          outputRange: ['0deg', '180deg'],
+        }),
+      },
+    ],
+  };
+  const flipToBackStyle = {
+    transform: [
+      {
+        rotateY: flipAnimation.interpolate({
+          inputRange: [0, 180],
+          outputRange: ['180deg', '360deg'],
+        }),
+      },
+    ],
+  };
+
+  const flipToFront = () => {
+    Animated.timing(flipAnimation, {
+      toValue: 180,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
+  const flipToBack = () => {
+    Animated.timing(flipAnimation, {
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
+
   return (
     <CardWrapper
-      onPress={onCardPress}
-      testID={`card-${card.id}`}
-      isOpen={isOpen}>
-      <CardContent testID={`card-content-${card.id}`} isOpen={isOpen}>
-        {isOpen ? card.number : '?'}
-      </CardContent>
+      onPress={() => {
+        onCardPress();
+        flipRotation ? flipToBack() : flipToFront();
+      }}>
+      <CardFront style={flipToBackStyle} testID={`card-${card.id}`}>
+        <CardContentFront testID={`card-content-${card.id}`}>
+          {'?'}
+        </CardContentFront>
+      </CardFront>
+      <CardBack style={flipToFrontStyle} testID={`card-${card.id}`}>
+        <CardContentBack testID={`card-content-${card.id}`}>
+          {card.number}
+        </CardContentBack>
+      </CardBack>
     </CardWrapper>
   );
 };
